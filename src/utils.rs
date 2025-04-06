968f8df4753f3f4d96c512529a87d8428bb293bd
aagbotemi <aabiodunawoyemi@gmail.com>
2025-04-06 22:18:47 +0100

    to_spend transaction function

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,7 @@
-use bitcoin::hashes::{Hash, HashEngine, sha256};
+use alloc::vec;
+use bitcoin::{
+    absolute::LockTime, hashes::{sha256, Hash, HashEngine}, opcodes::OP_0, script::Builder, transaction::Version, Amount, OutPoint, ScriptBuf, Sequence, Transaction, TxIn, TxOut, Txid, Witness
+};
 
 #[derive(Debug, PartialEq)]
 pub enum SignatureFormat {
@@ -18,4 +21,33 @@ pub fn tagged_message_hash(message: &[u8]) -> sha256::Hash {
     engine.input(message);
 
     sha256::Hash::from_engine(engine)
+}
+
+pub fn to_spend(script_pubkey: &ScriptBuf, message: &str) -> Transaction {
+    let txid = Txid::from_slice(&[0u8; 32]).expect("Txid slice error");
+
+    let outpoint = OutPoint {
+        txid,
+        vout: 0xFFFFFFFF,
+    };
+    let message_hash = tagged_message_hash(message.as_bytes());
+    let script_sig = Builder::new()
+        .push_opcode(OP_0)
+        .push_slice(message_hash.to_byte_array())
+        .into_script();
+
+    Transaction {
+        version: Version(0),
+        lock_time: LockTime::ZERO,
+        input: vec![TxIn {
+            previous_output: outpoint,
+            script_sig,
+            sequence: Sequence::ZERO,
+            witness: Witness::new(),
+        }],
+        output: vec![TxOut {
+            value: Amount::from_sat(0),
+            script_pubkey: script_pubkey.clone(),
+        }],
+    }
 }
