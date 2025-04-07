c7928126d37cff515292b67dd9589ea1d6d8d0a2
aagbotemi <aabiodunawoyemi@gmail.com>
2025-04-07 07:57:52 +0100

    test: unit test for message hash, to_spend and to_sign

--- src/utils.rs
+++ src/utils.rs
@@ -100,3 +100,80 @@ pub fn to_sign(
 }
 
 pub(crate) type SecpCtx = Secp256k1<All>;
+
+#[cfg(test)]
+mod tests {
+    use bitcoin::Address;
+    use core::str::FromStr;
+
+    use super::*;
+
+    const HELLO_WORLD_MESSAGE: &str = "Hello World";
+    const SEGWIT_ADDRESS: &str = "bc1q9vza2e8x573nczrlzms0wvx3gsqjx7vavgkx0l";
+
+    #[test]
+    fn test_message_hashing() {
+        let empty_hash = tagged_message_hash(b"");
+        let hello_world_hash = tagged_message_hash(b"Hello World");
+
+        assert_eq!(
+            empty_hash.to_string(),
+            "c90c269c4f8fcbe6880f72a721ddfbf1914268a794cbb21cfafee13770ae19f1"
+        );
+        assert_eq!(
+            hello_world_hash.to_string(),
+            "f0eb03b1a75ac6d9847f55c624a99169b5dccba2a31f5b23bea77ba270de0a7a"
+        );
+    }
+
+    #[test]
+    fn test_to_spend_and_to_sign() {
+        let script_pubkey = Address::from_str(SEGWIT_ADDRESS)
+            .unwrap()
+            .assume_checked()
+            .script_pubkey();
+
+        // Test case for empty message - to_spend
+        let tx_spend_empty_msg = to_spend(&script_pubkey, "");
+        assert_eq!(
+            tx_spend_empty_msg.compute_txid().to_string(),
+            "c5680aa69bb8d860bf82d4e9cd3504b55dde018de765a91bb566283c545a99a7"
+        );
+
+        // Test case for "Hello World" - to_spend
+        let tx_spend_hello_world_msg = to_spend(&script_pubkey, HELLO_WORLD_MESSAGE);
+        assert_eq!(
+            tx_spend_hello_world_msg.compute_txid().to_string(),
+            "b79d196740ad5217771c1098fc4a4b51e0535c32236c71f1ea4d61a2d603352b"
+        );
+
+        // Test case for empty message - to_sign
+        let tx_sign_empty_msg = to_sign(
+            &tx_spend_empty_msg.output[0].script_pubkey,
+            tx_spend_empty_msg.compute_txid(),
+            tx_spend_empty_msg.lock_time,
+            tx_spend_empty_msg.input[0].sequence,
+            Some(tx_spend_empty_msg.input[0].witness.clone()),
+        )
+        .unwrap();
+        assert_eq!(
+            tx_sign_empty_msg.unsigned_tx.compute_txid().to_string(),
+            "1e9654e951a5ba44c8604c4de6c67fd78a27e81dcadcfe1edf638ba3aaebaed6"
+        );
+
+        // Test case for HELLO_WORLD_MESSAGE - to_sign
+        let tx_sign_hw_msg = to_sign(
+            &tx_spend_hello_world_msg.output[0].script_pubkey,
+            tx_spend_hello_world_msg.compute_txid(),
+            tx_spend_hello_world_msg.lock_time,
+            tx_spend_hello_world_msg.input[0].sequence,
+            Some(tx_spend_hello_world_msg.input[0].witness.clone()),
+        )
+        .unwrap();
+
+        assert_eq!(
+            tx_sign_hw_msg.unsigned_tx.compute_txid().to_string(),
+            "88737ae86f2077145f93cc4b153ae9a1cb8d56afa511988c149c5c8c9d93bddf"
+        );
+    }
+}
